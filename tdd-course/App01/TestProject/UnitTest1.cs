6cb8e575cf95217d0ef80af6226359335cfd4ba2
hirofurukawa23 <info@hirofurukawa.com>
2021-11-23 16:34:04 +0900

    テスト駆動開発コースの一番最初のアプリケーションを追加しました。

---
+++ tdd-course/App01/TestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var sample = new App01.Sample();
+            Assert.IsFalse(sample.ReturnTrue());
+        }
+    }
+}
